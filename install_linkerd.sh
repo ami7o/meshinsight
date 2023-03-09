0811f80d4feb876e2d998b390c47b3b88d0871c7
Ami Oka <amingo.oka@gmail.com>
2023-03-09 01:11:43 -0800

    update install linkerd script

--- install_linkerd.sh
+++ install_linkerd.sh
@@ -2,11 +2,10 @@
 cd $MESHINSIGHT_DIR
 kubectl version --short
 curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install | sh
-export PATH=$PATH:~/.linkerd2/bin
+echo "export PATH=$PATH:~/.linkerd2/bin" > ~/.bashrc
+source ~/.bashrc
 linkerd version
 linkerd check --pre
 linkerd install --crds | kubectl apply -f -
 linkerd install | kubectl apply -f -
 linkerd check
-
-
